f4b54b296f5b2e47ca084a1c4a62a896ddaa709a
abdullah gomah <abdullhgomah724@gmail.com>
2023-01-28 17:40:00 +0300

    third

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ var videoFilter = function(req, file, cb){
     cb(null, true);
 }
 
-var mergeupload = multer({storage:mergestorage, fileFilter: videoFilter});
+var mergeupload = multer({storage:mergestorage});
 var convertupload = multer({storage:convertstorage});
 
 app.use(bodyParser.urlencoded({extended:false}))
